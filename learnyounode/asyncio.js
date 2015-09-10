f6cb9e144bafaa74d4d684afd4509dc5b5adaddb
ddrawer14 <ddrawer14@yahoo.com>
2015-09-10 16:59:44 +0000

    async io completed

---
+++ learnyounode/asyncio.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var file = process.argv[2];
+
+fs.readFile(file, 'utf8', function(err, data) {
+    
+    var lines = data.split('\n').length - 1;
+        
+    console.log(lines);
+    
+})
