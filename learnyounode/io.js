208c0fde609358783a2d8880ba6029552e5f44b3
ddrawer14 <ddrawer14@yahoo.com>
2015-09-10 14:32:17 +0000

    io.js complete

--- learnyounode/io.js
+++ learnyounode/io.js
@@ -1,3 +1,12 @@
 
 var fs = require('fs');
 
+var filePath = process.argv[2];
+
+var fileContents = fs.readFileSync(filePath);
+
+var str = fileContents.toString();
+
+var strSplit = str.split('\n');
+
+console.log(strSplit.length - 1);
