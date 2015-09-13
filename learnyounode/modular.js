c741273f03f11a5eda202862317aa1f52e647752
ddrawer14 <ddrawer14@yahoo.com>
2015-09-13 17:38:23 -0400

    Added comments

--- learnyounode/modular.js
+++ learnyounode/modular.js
@@ -1,3 +1,4 @@
+
 // You now have the module.exports object in your module assigned to the filter variable. 
 var filter = require('./modu1');
 var dirname = process.argv[2];
@@ -7,7 +8,12 @@ var ext = process.argv[3];
 filter(dirname, ext, function(err, list) {
 	if (err) return err;
 
-	list.forEach(function(file) { 
+	// arr.forEach(callback[, thisArg])
+	 // forEach() method executes a provided callback function once per array element	 
+	
+	list.forEach( function(file) { 
+	// callback: Function to execute for each element, taking three arguments: (currentValue, index, array)
+
 		console.log(file);
 	})
 
